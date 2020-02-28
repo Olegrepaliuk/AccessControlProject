ee9189dca012986e60a7c454c9ac93778fca6317
Oleg <Oleg58807@ukr.net>
2020-02-28 23:22:08 +0200

    Fixed path to Update action; added UsersController

---
+++ AccessControl/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccessControl.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessControl.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            var simpleUsers = new List<User>();
+            foreach (var user in _userManager.Users)
+            {
+
+            }
+            return View(_userManager.Users.ToList());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var listRoles = await _userManager.GetRolesAsync(user);
+                if (listRoles.Where(l => l.ToUpper() == "ADMIN").Count() == 0)
+                {
+                    IdentityResult result = await _userManager.DeleteAsync(user);
+                }
+                
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- AccessControl/Models/Room.cs
+++ AccessControl/Models/Room.cs
@@ -12,6 +12,6 @@ namespace AccessControl.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public bool MainEntrance { get; set; }
+        //public bool MainEntrance { get; set; }
     }
 }
